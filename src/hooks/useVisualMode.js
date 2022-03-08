14003c0e2fed4b3a0217fbb98a4e1cd3104691b8
Hamid Nur <hamidsharif2001@gmail.com>
2022-03-07 19:46:42 -0500

    update use visual mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -11,28 +11,19 @@ export default function useVisualMode(initial){
     if(replace){
       setHistory(prev => prev.slice(0, -1));
       setHistory(prev => [...prev, nextMode]);
-
       }else{
-        setHistory(prev => [...prev, nextMode]);
+        setHistory(prev => [...prev, nextMode]); 
       }
       setMode(nextMode);
   }
 
   //back function
   function back() {
-    
     if(history.length > 1) {
-
-
-
-            setHistory(history.slice(0, -1));
-
-            setMode(history[history.length-2]);
-
-
+      setHistory(history.slice(0, -1));
+      setMode(history[history.length-2]);
     }
-    }
-
+  }
 
   return { mode, transition, back};
-} 
+}
